15f73ed275d51ab560bae8b9af910b6dbf34b33a
kelseydamas <kelseydamas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-12 23:33:19 +0000

    adding prev/next month links to grid view #7978
    git-svn-id: https://plugins.svn.wordpress.org/the-events-calendar/trunk@240381 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/gridview.php
+++ views/gridview.php
@@ -7,8 +7,21 @@
 	<div id="tec-content" class="grid">
 		<div id='tec-events-calendar-header' class="clearfix">
 			<h2 class="tec-cal-title"><?php _e('Calendar of Events', $spEvents->pluginDomain) ?></h2>
+			<span class='tec-month-nav'>
+				<span class='tec-prev-month'>
+					<a href='<?php echo events_get_previous_month_link(); ?>'>
+					&#x2190; <?php echo events_get_previous_month_text(); ?>
+					</a>
+				</span>
 
-			<?php get_jump_to_date_calendar( "tec-" ); ?>
+				<?php get_jump_to_date_calendar( "tec-" ); ?>
+	
+				<span class='tec-next-month'>
+					<a href='<?php echo events_get_next_month_link(); ?>'>				
+					<?php echo events_get_next_month_text(); ?> &#x2192; 
+					</a>
+				</span>
+			</span>
 
 			<span class='tec-calendar-buttons'> 
 				<a class='tec-button-off' href='<?php echo events_get_listview_link(); ?>'><?php _e('Event List', $spEvents->pluginDomain)?></a>
